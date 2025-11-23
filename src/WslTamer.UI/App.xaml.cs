8a0b70982c7a76909ada33719d0e54e881753a77
Ryan Haver <ryan.haver@gmail.com>
2025-11-23 13:30:44 -0700

    Initial commit: Project structure and README

---
+++ src/WslTamer.UI/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using Forms = System.Windows.Forms;
+using System.Drawing;
+
+namespace WslTamer.UI;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : System.Windows.Application
+{
+    private Forms.NotifyIcon? _notifyIcon;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        _notifyIcon = new Forms.NotifyIcon();
+        // Use a standard system icon for now since we don't have a custom .ico yet
+        _notifyIcon.Icon = SystemIcons.Application; 
+        _notifyIcon.Visible = true;
+        _notifyIcon.Text = "WSL Tamer";
+
+        var contextMenu = new Forms.ContextMenuStrip();
+        contextMenu.Items.Add("Exit", null, (s, args) => Shutdown());
+
+        _notifyIcon.ContextMenuStrip = contextMenu;
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        _notifyIcon?.Dispose();
+        base.OnExit(e);
+    }
+}
+
